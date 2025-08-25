abecff9fe701ac7aa071bfd8b100ef2389eae35f
Aaron Smith <arsmith@mit.edu>
2025-08-25 12:11:00 -0400

    movie

---
+++ candidates/jobs_movie/job-movie.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# SBATCH --job-name=colt
+#SBATCH --output=movie-%a-%A.out
+# SBATCH --array=87
+#SBATCH --array=0-103
+# SBATCH --array=8-188:10
+# SBATCH --array=108-188:20
+# SBATCH --array=188
+# SBATCH --partition=sched_mit_mvogelsb,sched_mit_mki,sched_mit_mki_preempt
+#SBATCH --partition=sched_mit_mki,sched_mit_mki_preempt
+# SBATCH --partition=sched_mit_mvogelsb,sched_mit_mki
+# SBATCH --partition=sched_mit_mki,sched_mit_mki_preempt
+# SBATCH --partition=sched_mit_mki_r8
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64
+# SBATCH --cpus-per-task=32
+# SBATCH --cpus-per-task=16
+# SBATCH --cpus-per-task=8
+#SBATCH --constraint=centos7
+#SBATCH --mem-per-cpu=6000 # 6GB of memory per CPU
+# SBATCH --mem-per-cpu=3000 # 3GB of memory per CPU
+#SBATCH --export=ALL
+# SBATCH --time=168:00:00
+# SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
+# SBATCH --time=12:00:00
+# SBATCH --time=6:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=arsmith@mit.edu
+#SBATCH --exclude=node1447,node1445
+# SBATCH --exclude=node1400,node1406,node1414,node1418,node1454,node1456
+# node1400,node1406,node1407,node1408,node1409,node1420,node1422,node1423,node1424,node1427,node1436,node1438,node1439,node1441,node1444,node1450
+# ###SBATCH --exclude=node1412,node1413,node1415,node1421,node1426,node1454,node1455,node1447
+# SBATCH --dependency=afterok:3354056
+
+## Module setup
+. /etc/profile.d/modules.sh
+source ~/.colt
+
+## OpenMP threads
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export HDF5_USE_FILE_LOCKING=FALSE
+
+call="mpirun --mca opal_warn_on_missing_libcuda 0 --mca btl ^openib --mca psm2 ucx -np $SLURM_NTASKS --bind-to none"
+colt="${HOME}/colt/colt"
+#colt="${HOME}/colt/colt-long"
+#
+# colt="${HOME}/colt/colt-flows"
+# colt="${HOME}/colt/colt-long-flows"
+sim=${group}/${run}
+
+setup_runs() {
+    echo "Setting up simulation ${sim} ..."
+    ## time python distances.py $sim $SLURM_ARRAY_TASK_ID         # Low/high-res distances
+    ## time python candidates.py $sim $SLURM_ARRAY_TASK_ID        # Uncontaminated candidates
+    ## time python halos.py $sim $SLURM_ARRAY_TASK_ID             # Write halos to file
+    ## time python arepo_to_colt.py $sim $SLURM_ARRAY_TASK_ID     # Arepo to COLT initial conditions
+    ## $call $colt config-connect.yaml $SLURM_ARRAY_TASK_ID       # Run COLT to get connections (without circulators)
+    ## time python remove_lowres.py $sim $SLURM_ARRAY_TASK_ID     # Remove exterior low-res particles
+    ## $call $colt config-connect-circ.yaml $SLURM_ARRAY_TASK_ID  # Rerun for updated connections (with circulators)
+    # for snap in {0..189}; do
+    #     # echo "Running halo ${snap} ..."
+    #     python candidates.py $sim $snap
+    #     python halos.py $sim $snap
+    # done
+}
+
+run() {
+    for suffix in "$@"; do
+        config_file="config-${suffix}.yaml"
+        #echo "Running simulation with ${config_file} ..."
+        #$call $colt $config_file $SLURM_ARRAY_TASK_ID
+        # $call $colt-flows $config_file $SLURM_ARRAY_TASK_ID
+        # $call $colt-ion $config_file $SLURM_ARRAY_TASK_ID
+        $call $colt-line $config_file $SLURM_ARRAY_TASK_ID
+        # $call $colt-ion-teq $config_file $SLURM_ARRAY_TASK_ID
+        # $call $colt-line-teq $config_file $SLURM_ARRAY_TASK_ID
+    done
+}
+
+# $call $colt-line config-halo-Ha.yaml $SLURM_ARRAY_TASK_ID
+# $call $colt-line config-halo-OII-3727-3730.yaml $SLURM_ARRAY_TASK_ID
+# $call $colt-line config-halo-OIII-5008.yaml $SLURM_ARRAY_TASK_ID
+# $call $colt-flows config-halo-ion-eq.yaml $SLURM_ARRAY_TASK_ID
+
+# $call $colt-tree-proj config-movie-proj-rho.yaml $SLURM_ARRAY_TASK_ID
+# $call $colt-tree-proj config-movie-proj-all.yaml $SLURM_ARRAY_TASK_ID
+
+if [ $SLURM_ARRAY_TASK_ID -gt 0 ]; then
+    for sub in 0 1 2 3 4 5 6 7 8 9; do
+        $call $colt-tree-proj config-movie-proj-rho.yaml $SLURM_ARRAY_TASK_ID$sub
+    done
+else
+    for sub in 0 1 2 3 4 5 6 7 8 9; do
+        $call $colt-tree-proj config-movie-proj-rho.yaml $sub
+    done
+    # Remainder files
+    for sub in 0 1 2 3; do
+        $call $colt-tree-proj config-movie-proj-rho.yaml 104$sub
+    done
+fi
+
+# setup_runs
+# run ion-eq-pre7
+# run ion-eq-pre8
+# run ion-eq
+# run ion-eq-MCRT ion-eq-RHD
+# run proj Ha Lya
+# run halo-ion-eq-RHD
+#run halo-M1500-RHD
+#run halo-optical
+#run halo-Ha-RHD
+# run Lya-RHD
+# run halo-Ha halo-OII-3727-3730 halo-OIII-5008
+# run halo-OII-3727-3730
+# run halo-Ha halo-OIII-5008
+# run OII-3727-3730 OIII-5008 M1500
+#run ion-eq-pre7-teq ion-eq-pre8-teq
+# run halo-Ha-teq halo-OIII-5008-teq
+
---
+++ candidates/jobs_movie/submit-movie.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Python list of simulations to run
+# sims = {
+#   'g39': ['z4'],
+#   'g205': ['z4'],
+#   'g578': ['z4'],
+#   'g1163': ['z4', 'noESF_z4'],
+#   'g5760': ['z4', 'varEff_z4', 'noRlim_z4', 'noESF_z4', 'z8'],
+#   'g10304': ['z4', 'noESF_z4', 'z8'],
+#   'g33206': ['z4', 'varEff_z4', 'noRlim_z4', 'noESF_z4', 'z8', 'varEff_z8', 'noESF_z8', 'noRlim_z8', 'noExt_z8', 'uvb_z8'],
+#   'g37591': ['z4', 'z8', 'uvb_z8'],
+#   'g137030': ['z4', 'varEff_z4', 'noRlim_z4', 'noESF_z4', 'z8', 'varEff_z8', 'noESF_z8', 'noRlim_z8', 'noExt_z8', 'uvb_z8'],
+#   'g500531': ['z4', 'z8', 'uvb_z8'],
+#   'g519761': ['z4', 'z8', 'uvb_z8'],
+#   'g2274036': ['z4', 'z8', 'uvb_z8'],
+#   'g5229300': ['z4', 'z8', 'uvb_z8'],
+# }
+
+# Fiducial test case
+# job='F'; group='g5760'; runs=('z4')
+
+# Bash list of simulations to run
+# job='B'; group='g39'; runs=('z4')
+# job='C'; group='g205'; runs=('z4')
+# job='D'; group='g578'; runs=('z4')
+# job='E'; group='g1163'; runs=('z4' 'noESF_z4')
+# job='F'; group='g5760'; runs=('z4' 'varEff_z4' 'noRlim_z4' 'noESF_z4' 'z8')
+# job='G'; group='g10304'; runs=('z4' 'noESF_z4' 'z8')
+# job='H'; group='g33206'; runs=('z4' 'varEff_z4' 'noRlim_z4' 'noESF_z4' 'z8' 'varEff_z8' 'noESF_z8' 'noRlim_z8' 'noExt_z8' 'uvb_z8')
+# job='I'; group='g37591'; runs=('z4' 'z8' 'noExt_z8' 'uvb_z8')
+# job='J'; group='g137030'; runs=('z4' 'varEff_z4' 'noRlim_z4' 'noESF_z4' 'z8' 'varEff_z8' 'noESF_z8' 'noRlim_z8' 'noExt_z8' 'uvb_z8')
+# job='K'; group='g500531'; runs=('z4' 'z8' 'noExt_z8' 'uvb_z8')
+# job='L'; group='g519761'; runs=('z4' 'z8' 'noExt_z8' 'uvb_z8')
+# job='M'; group='g2274036'; runs=('z4' 'z8' 'noExt_z8' 'uvb_z8')
+# job='N'; group='g5229300'; runs=('z4' 'z8' 'noExt_z8' 'uvb_z8')
+
+# Define job sets with individual job, group, and runs
+job_sets=(
+# COLT (long circulators)
+#"job='D'; group='g578'; runs=('noESF_z4'); first_snap=88; last_snap=179"       # 88-179 ->  88-179 [2e9]
+#"job='D'; group='g578'; runs=('varEff_z4'); first_snap=0; last_snap=68"        # 0-68   ->  0-68
+#"job='E'; group='g1163'; runs=('noESF_z4'); first_snap=0; last_snap=188"       # 0-188  ->  0-188  [2e9]
+#"job='H'; group='g33206'; runs=('z16'); first_snap=0; last_snap=8"             # 0-8    ->  0-8
+# Fiducial:
+#X"job='A'; group='g2'; runs=('z4'); first_snap=0; last_snap=67"                 # 0-67   ->  0-67
+#X"job='B'; group='g39'; runs=('z4'); first_snap=0; last_snap=188"               # 0-188  ->  0-188
+# "job='C'; group='g205'; runs=('z4'); first_snap=0; last_snap=188"              # 0-188  ->  0-188  [2e9] long circ 128-188
+# "job='D'; group='g578'; runs=('z4'); first_snap=0; last_snap=188"              # 0-188  ->  0-188  [2e9] long circ 170-188
+# "job='E'; group='g1163'; runs=('z4'); first_snap=0; last_snap=188"             # 0-188  ->  0-188  [2e9]
+# "job='F'; group='g5760'; runs=('z8'); first_snap=0; last_snap=188"             # 0-188  ->  0-188  [1e9] long circ 155-188
+# "job='G'; group='g10304'; runs=('z8'); first_snap=0; last_snap=188"            # 0-188  ->  0-188  [1e9]
+# "job='J'; group='g137030'; runs=('z16'); first_snap=5; last_snap=188"          # 0-188  ->  5-188  [1e9]
+# "job='K'; group='g500531'; runs=('z16'); first_snap=9; last_snap=188"          # 0-188  ->  9-188  [8e8]
+# "job='L'; group='g519761'; runs=('z16'); first_snap=39; last_snap=188"         # 0-188  ->  39-188 [6e8]
+# "job='M'; group='g2274036'; runs=('z16'); first_snap=9; last_snap=188"         # 0-189  ->  9-188  [4e8]
+#
+# COLT (int circulators)
+#"job='F'; group='g5760'; runs=('noESF_z4'); first_snap=5; last_snap=188"       # 0-188  ->  5-188  [8e8]
+#"job='G'; group='g10304'; runs=('noESF_z4'); first_snap=0; last_snap=188"      # 0-188  ->  0-188  [8e8]
+#"job='H'; group='g33206'; runs=('noESF_z8'); first_snap=4; last_snap=188"      # 0-188  ->  4-188  [4e8]
+#"job='H'; group='g33206'; runs=('noESF_z4'); first_snap=6; last_snap=188"      # 0-188  ->  6-188  [4e8]
+#"job='J'; group='g137030'; runs=('noESF_z8'); first_snap=2; last_snap=188"     # 0-188  ->  2-188  [2e8]
+#"job='J'; group='g137030'; runs=('noESF_z4'); first_snap=10; last_snap=188"    # 0-188  ->  10-188 [2e8]
+#
+# "job='F'; group='g5760'; runs=('noRlim_z4'); first_snap=5; last_snap=188"      # 0-188  ->  5-188
+# "job='F'; group='g5760'; runs=('varEff_z4'); first_snap=5; last_snap=188"      # 0-188  ->  5-188
+# "job='H'; group='g33206'; runs=('uvb_z8'); first_snap=4; last_snap=188"        # 0-188  ->  4-188
+# "job='H'; group='g33206'; runs=('noExt_z8'); first_snap=4; last_snap=188"      # 0-188  ->  4-188
+# "job='H'; group='g33206'; runs=('noRlim_z8'); first_snap=4; last_snap=188"     # 0-188  ->  4-188
+# "job='H'; group='g33206'; runs=('varEff_z8'); first_snap=3; last_snap=188"     # 0-188  ->  3-188
+# "job='H'; group='g33206'; runs=('noRlim_z4'); first_snap=6; last_snap=189"     # 0-189  ->  6-189
+# "job='H'; group='g33206'; runs=('varEff_z4'); first_snap=6; last_snap=189"     # 0-189  ->  6-189
+# "job='I'; group='g37591'; runs=('uvb_z8'); first_snap=7; last_snap=188"        # 0-188  ->  7-188
+# "job='I'; group='g37591'; runs=('noExt_z8'); first_snap=6; last_snap=188"      # 0-188  ->  6-188
+# "job='J'; group='g137030'; runs=('uvb_z8'); first_snap=3; last_snap=188"       # 0-188  ->  3-188
+# "job='J'; group='g137030'; runs=('noExt_z8'); first_snap=3; last_snap=188"     # 0-188  ->  3-188
+# "job='J'; group='g137030'; runs=('noRlim_z8'); first_snap=2; last_snap=188"    # 0-188  ->  2-188
+# "job='J'; group='g137030'; runs=('varEff_z8'); first_snap=3; last_snap=188"    # 0-188  ->  3-188
+# "job='J'; group='g137030'; runs=('noRlim_z4'); first_snap=10; last_snap=189"   # 0-189  ->  10-189
+# "job='J'; group='g137030'; runs=('varEff_z4'); first_snap=10; last_snap=188"   # 0-188  ->  10-188
+# "job='K'; group='g500531'; runs=('uvb_z8'); first_snap=8; last_snap=189"       # 0-189  ->  8-189
+# "job='K'; group='g500531'; runs=('noExt_z8'); first_snap=8; last_snap=188"     # 0-188  ->  8-188
+# "job='L'; group='g519761'; runs=('uvb_z8'); first_snap=23; last_snap=189"      # 0-189  ->  23-189
+# "job='L'; group='g519761'; runs=('noExt_z8'); first_snap=46; last_snap=189"    # 0-189  ->  46-189
+# "job='M'; group='g2274036'; runs=('uvb_z8'); first_snap=22; last_snap=182"     # 0-189  ->  22-182
+# "job='M'; group='g2274036'; runs=('noExt_z8'); first_snap=28; last_snap=189"   # 0-189  ->  28-189
+# "job='N'; group='g5229300'; runs=('uvb_z8'); first_snap=0; last_snap=0"        # 0-189  ->  0-0
+# "job='N'; group='g5229300'; runs=('noExt_z8'); first_snap=0; last_snap=0"      # 0-189  ->  0-0
+# Fiducial:
+# "job='F'; group='g5760'; runs=('z4'); first_snap=5; last_snap=188"             # 0-188  ->  5-188  [8e8]
+"job='G'; group='g10304'; runs=('z4'); first_snap=0; last_snap=188"            # 0-188  ->  0-188  [8e8]
+# "job='H'; group='g33206'; runs=('z8'); first_snap=4; last_snap=188"            # 0-188  ->  4-188  [4e8]
+# "job='H'; group='g33206'; runs=('z4'); first_snap=6; last_snap=189"            # 0-189  ->  6-189  [4e8]
+# "job='I'; group='g37591'; runs=('z8'); first_snap=6; last_snap=188"            # 0-188  ->  6-188  [4e8]
+# "job='I'; group='g37591'; runs=('z4'); first_snap=11; last_snap=188"           # 0-188  ->  11-188 [4e8]
+# "job='J'; group='g137030'; runs=('z8'); first_snap=2; last_snap=188"           # 0-188  ->  2-188  [2e8]
+# "job='J'; group='g137030'; runs=('z4'); first_snap=10; last_snap=188"          # 0-188  ->  10-188 [2e8]
+# "job='K'; group='g500531'; runs=('z8'); first_snap=10; last_snap=188"          # 0-188  ->  10-188 [1e8]
+#"job='K'; group='g500531'; runs=('z4'); first_snap=16; last_snap=189"          # 0-189  ->  16-189 [1e8]
+# "job='L'; group='g519761'; runs=('z8'); first_snap=46; last_snap=189"          # 0-189  ->  46-189 [1e8]
+# "job='L'; group='g519761'; runs=('z4'); first_snap=51; last_snap=189"          # 0-189  ->  51-189 [1e8]
+# "job='M'; group='g2274036'; runs=('z8'); first_snap=27; last_snap=189"         # 0-189  ->  27-189 [1e8]
+# "job='M'; group='g2274036'; runs=('z4'); first_snap=52; last_snap=189"         # 0-189  ->  52-189 [1e8]
+# "job='N'; group='g5229300'; runs=('z16'); first_snap=72; last_snap=189"        # 0-189  ->  72-189 [1e8]
+# "job='N'; group='g5229300'; runs=('z8'); first_snap=83; last_snap=189"         # 0-189  ->  83-189 [1e8]
+# "job='N'; group='g5229300'; runs=('z4'); first_snap=87; last_snap=189"         # 0-189  ->  87-189 [1e8]
+)
+
+copy_dir=$PWD
+zoom_dir=/orcd/data/mvogelsb/004/Thesan-Zooms
+base_dir=${zoom_dir}-COLT
+#base_dir=/orcd/data/mvogelsb/005/Lab/Thesan-Zooms-COLT
+
+# Loop through job sets
+for job_set in "${job_sets[@]}"; do
+  eval "$job_set"  # Evaluate the job set to extract job, group, and runs
+
+  # Loop through the run directories
+  for run in "${runs[@]}"; do
+    echo "Job ${job}, Group ${group}, Run ${run}"
+    cd $copy_dir  # Reset path
+    # post_dir=${zoom_dir}/${group}/${run}/postprocessing
+    colt_dir=${base_dir}/${group}/${run}
+
+    cp config-movie*.yaml ${colt_dir}/.
+    cp job-movie.sh ${colt_dir}/.
+
+    cd ${colt_dir}
+    pwd
+
+    export group run
+    sbatch -J ${job}_${run} job-movie.sh
+  done
+done
+
